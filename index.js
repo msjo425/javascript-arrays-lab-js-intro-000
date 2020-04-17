45149628385b2c7bbcf7bf5a3c10a3f4b5fed85c
matt joseloff <msjo425@gmail.com>
2020-04-17 21:24:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function appendKitten(newKitten){
   }
 
 function prependKitten(newKitten){
-  newArray = [newKitten, ...]
+  newArray = [newKitten, ...kittens]
   return newKitten.concat(kittens)
 }
