388c4b85efbe8247217c71c8e9f9a76995fd1c60
matt joseloff <msjo425@gmail.com>
2020-04-17 20:58:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-var kittens = [] //define your array here
+var kittens = ["Milo","Otis","Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(newKitten){
+  kittens.push(newKitten)
+}
+
+function destructivelyPrependKitten(newKitten){
+  kittens.unshift(newKitten)
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+function appendKitten(newKitten){
+  return kittens.concat(newKitten)
+  }
+
+function prependKitten(newKitten){
+  return newKitten.concat(kittens)
+}
